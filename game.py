b33a034180be3d9b50b4d1b2e9f13d677de48f13
Abdullah21gg <abdullahgaming7600@gmail.com>
2026-08-16 13:08:56 -0700

    Add files via upload

---
+++ game.py
@@ -0,0 +1,16 @@
+import random
+secret_number = random.randint(1,10)
+attempts = 0
+while True:
+    try:
+        guess = int(input("guess a number between 1 and 10: "))
+        attempts += 1
+        if guess == secret_number:
+            print ("you win")
+            break
+        elif guess < secret_number:
+            print ("too low, try again")
+        else:
+            print ("too high, try again")
+    except ValueError:
+        print ("please enter a valid number.")
